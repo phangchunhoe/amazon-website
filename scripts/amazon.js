f1d8c04110af6a83d87c314052bb631d1a0d93a2
chunho <chunhoes.inbox@gmail.com>
2025-10-10 18:23:35 +0800

    allowed the add-to-cart div to pop up when button is clicked

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -56,11 +56,16 @@ products.forEach((product) => {
 
 document.querySelector('.products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button, i) => {
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
+        const quantityValue = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+        const addedToCart = button.closest('.product-container')?.querySelector('.added-to-cart');
 
         let matchingItem;
+        let totalItem = 0;
+
+        // Check whether there is matching items
         cart.forEach((item) => {
             if (productId === item.Id) {
                 matchingItem = item;
@@ -68,16 +73,36 @@ document.querySelectorAll('.js-add-to-cart').forEach((button, i) => {
         });
 
         if (matchingItem) {
-            matchingItem.quantity += 1;
+            matchingItem.quantity += quantityValue;
         }
         else {
+            // Else, make a new index for it
             cart.push({
                 Id: productId,
-                quantity: 1
+                quantity: quantityValue
             });  
         }
-
-        
         console.log(cart)
+
+        // Find the total number of items in cart
+        cart.forEach((item) => {
+            totalItem += item.quantity;
+        })
+
+        if (totalItem) {
+            document.querySelector('.cart-quantity').innerText = totalItem;
+        } else {
+            document.querySelector('.cart-quantity').innerText = '';
+        };
+
+        // Reset numbers back to their original form
+        document.querySelector(`.js-quantity-selector-${productId}`).value = 1;
+
+        // Display 'Added' check
+        addedToCart.classList.add('is-added-to-cart');
+
+        setTimeout(() => {
+            addedToCart.classList.remove('is-added-to-cart');
+        }, 1500);
     })
 })
