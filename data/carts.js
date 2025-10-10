24a7b963c85221f047f8853cb37e088454c96eab
chunho <chunhoes.inbox@gmail.com>
2025-10-10 21:05:34 +0800

    added the js file for the checkout page

--- data/carts.js
+++ data/carts.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+    Id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+}, {
+    Id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}];
 
 export function addToCart(matchingItem, productId, quantityValue) {
     // Check whether there is matching items
