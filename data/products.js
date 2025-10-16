c944d02694ed440d1191352e01c3c748a02b2dcf
chunho <chunhoes.inbox@gmail.com>
2025-10-16 14:48:36 +0800

    used inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -20,25 +20,46 @@ class Product {
   getPrice() {
     return `$${String((Math.round(this.priceCents) / 100).toFixed(2))}`;
   }
-}
 
-const product1 = new Product(  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  extraInfoHTML() {
+    return '';
+  }
+};
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  };
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target="_blank" class="size-chart">Size Chart</a>
+    `;
+  }
+};
+
+const tshirt = new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
       stars: 4.5,
-      count: 87
+      count: 56
     },
-    priceCents: 1090,
+    priceCents: 799,
     keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
-);
-console.log(product1);
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  });
+
+console.log(tshirt);
 
 export function getProduct(productId) {
   let matchingProduct;
@@ -712,5 +733,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
-  return new Product(productDetails);
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  } 
+  else return new Product(productDetails);
 });
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -33,19 +33,21 @@ products.forEach((product) => {
 
           <div class="product-quantity-container">
             <select class="js-quantity-selector-${product.id}">
-				<option selected value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-				<option value="4">4</option>
-				<option value="5">5</option>
-				<option value="6">6</option>
-				<option value="7">7</option>
-				<option value="8">8</option>
-				<option value="9">9</option>
-				<option value="10">10</option>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
             </select>
           </div>
 
+          ${product.extraInfoHTML()}
+          
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
